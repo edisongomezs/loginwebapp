c00d8029eea8429339f9c2d5d32ea700686f088e
egomezs <edisonjaviergomezs@gmail.com>
2024-07-02 22:17:23 -0500

    update to fix problem app to bdd

--- sql/init.sql
+++ sql/init.sql
@@ -1,13 +1,12 @@
 CREATE DATABASE IF NOT EXISTS sample;
- 
 USE sample;
- 
+
 CREATE TABLE IF NOT EXISTS USER (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    regdate DATE NOT NULL
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    username VARCHAR(50),
+    password VARCHAR(50),
+    regdate DATE
 );
