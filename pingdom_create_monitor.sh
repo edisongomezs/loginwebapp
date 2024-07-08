6252cf022d8afdf6879845a5568585f47411d6d3
egomezs <edisonjaviergomezs@gmail.com>
2024-07-08 13:21:38 -0500

    cambios para no generar un nuevo pingdom si ya existe

--- pingdom_create_monitor.sh
+++ pingdom_create_monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Configuración
-API_KEY="L-zklezObcdC4KFPaMfwIp-bOrNVFW6i25Nf-rsypCqODvDa5Z85N2tPP1QY1E-trwCGQ_M"
+API_KEY="PV897FeJbpu9e5brlSaV484jZBwXDbBKdEQGhEPiHAHlm4pCXUpuvwKH8TYjNFz66V0Djao"
 HOST="af1ff656414644715909119ba19bb800-1499347685.us-east-1.elb.amazonaws.com"
 CHECK_NAME="WebApp Login"
 INTERVAL=1  # Intervalo en minutos
@@ -13,24 +13,46 @@ echo "CHECK_NAME: $CHECK_NAME"
 echo "INTERVAL: $INTERVAL"
 
 # Verificar si el monitor ya existe
-EXISTING_CHECK=$(curl -s -X GET "https://api.pingdom.com/api/3.1/checks" \
+GET_URL="https://api.pingdom.com/api/3.1/checks"
+echo "Solicitud GET a: $GET_URL"
+RESPONSE=$(curl -s -X GET "$GET_URL" \
     -H "Authorization: Bearer $API_KEY" \
-    -H "Content-Type: application/json" | jq -r '.checks[] | select(.name == "'"$CHECK_NAME"'") | .id')
+    -H "Accept: application/json")
+
+echo "Respuesta de la solicitud GET: $RESPONSE"
+
+EXISTING_CHECK=$(echo "$RESPONSE" | jq -r '.checks[]? | select(.name == "'"$CHECK_NAME"'") | .id')
 
 if [ -n "$EXISTING_CHECK" ]; then
     echo "El monitor '$CHECK_NAME' ya existe con ID: $EXISTING_CHECK"
 else
     # Crear Monitor en Pingdom
-    RESPONSE=$(curl -s -X POST "https://api.pingdom.com/api/3.1/checks" \
+    PAYLOAD=$(cat <<EOF
+{
+  "name": "$CHECK_NAME",
+  "host": "$HOST",
+  "type": "http",
+  "url": "/LoginWebApp/index.jsp",
+  "port": 8080,
+  "resolution": $INTERVAL
+}
+EOF
+)
+
+    echo "Payload de la solicitud POST: $PAYLOAD"
+    
+    POST_URL="https://api.pingdom.com/api/3.1/checks"
+    RESPONSE=$(curl -s -X POST "$POST_URL" \
         -H "Authorization: Bearer $API_KEY" \
         -H "Content-Type: application/json" \
-        -d '{
-              "name": "'"$CHECK_NAME"'",
-              "host": "'"$HOST"'",
-              "type": "http",
-              "url": "/LoginWebApp/index.jsp",
-              "port": 8080,
-              "resolution": '"$INTERVAL"'
-            }')
+        -d "$PAYLOAD")
+
+    echo "Respuesta de la solicitud POST: $RESPONSE"
+
+    if echo "$RESPONSE" | jq -e '.error' >/dev/null; then
+        echo "Error al crear el monitor en Pingdom: $(echo "$RESPONSE" | jq -r '.error.errormessage')"
+        exit 1
+    fi
+
     echo "Monitor creado: $RESPONSE"
 fi
