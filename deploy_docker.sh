efa0acf186712711d05bd1d97feceb8a223ea6ae
egomezs <edisonjaviergomezs@gmail.com>
2024-07-08 16:11:46 -0500

    Usar variables de entorno para Docker credentials

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -4,11 +4,21 @@
 IMAGE_NAME="edisongomezs/loginwebapp"
 TAG="latest"
 DOCKER_USERNAME="edisongomezs"
-DOCKER_PASSWORD="Jeremias**3383"
+DOCKER_PASSWORD="${DOCKER_PASSWORD}"
 
-# Build the Docker image
+# Detener y eliminar el contenedor anterior si existe
+docker stop docker_web || true
+docker rm docker_web || true
+
+# Eliminar la imagen anterior
+docker rmi $IMAGE_NAME:$TAG || true
+
+# Construir la nueva imagen
 docker build -t $IMAGE_NAME:$TAG .
 
-# Push the Docker image
+# Iniciar sesión en Docker Hub y empujar la imagen
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 docker push $IMAGE_NAME:$TAG
+
+# Ejecutar el contenedor con la nueva imagen
+docker run -d --name docker_web -p 8080:8080 $IMAGE_NAME:$TAG
